107e404a8d2fdcae4353997f87749d6a64befdf6
tebacho <rodolfoyanezastudillo@gmail.com>
2015-11-29 01:00:04 -0300

    correccion propiedadVO

--- Common/src/com/immobilis/vo/PropiedadVO.java
+++ Common/src/com/immobilis/vo/PropiedadVO.java
@@ -9,109 +9,98 @@ import java.util.Map;
  */
 public abstract class PropiedadVO {
 	
-	private String idPropiedad;
-	private PropietarioVO propietario;
-	private String descripcion;
-	private String proyecto ;
-	private TipoPropiedad tipoPropiedad;
-	private String direccion;
-	private TipoOperacion tipoOperacion;
-	private Map<String,String> urlImagenes;
+	private int idPropiedad;
+	private ComunaVO comuna;
 	private ConstructoraVO constructora;
+	TipoPropiedad tipoPropiedad;
+	private String descripcion;
+	private TipoOperacion tipoOperacion;
+	private String disponible;
+	private String proyecto ;
 	private int metrosCuadrados;
 	private int precioUF;
-	
-	
-	
-	public int getPrecioUF() {
-		return precioUF;
-	}
+	private String direccion;
+	private PropietarioVO propietario;	
+	private Map<String,String> urlImagenes;
 
-	public void setPrecioUF(int precioUF) {
-		this.precioUF = precioUF;
+	public int getIdPropiedad() {
+		return idPropiedad;
 	}
-
-	public int getMetrosCuadrados() {
-		return metrosCuadrados;
+	public void setIdPropiedad(int idPropiedad) {
+		this.idPropiedad = idPropiedad;
 	}
-
-	public void setMetrosCuadrados(int metrosCuadrados) {
-		this.metrosCuadrados = metrosCuadrados;
+	public ComunaVO getComuna() {
+		return comuna;
 	}
-
+	public void setComuna(ComunaVO comuna) {
+		this.comuna = comuna;
+	}
 	public ConstructoraVO getConstructora() {
 		return constructora;
 	}
-
 	public void setConstructora(ConstructoraVO constructora) {
 		this.constructora = constructora;
 	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public String getIdPropiedad() {
-		return idPropiedad;
-	}
-
-	public void setIdPropiedad(String idPropiedad) {
-		this.idPropiedad = idPropiedad;
-	}
-
-	public void setUrlImagenes(Map<String, String> urlImagenes) {
-		this.urlImagenes = urlImagenes;
-	}
-
-	public Map<String, String> getUrlImagenes() {
-		return urlImagenes;
-	}
-
-	public void setUrlDetalles(Map<String, String> urlImagenes) {
-		this.urlImagenes = urlImagenes;
-	}
-
-	public PropietarioVO getPropietario() {
-		return propietario;
-	}
-
-	public void setPropietario(PropietarioVO propietario) {
-		this.propietario = propietario;
-	}
-
-	public String getProyecto() {
-		return proyecto;
-	}
-
-	public void setProyecto(String proyecto) {
-		this.proyecto= proyecto;
-	}
-	
 	public TipoPropiedad getTipoPropiedad() {
 		return tipoPropiedad;
 	}
-
 	public void setTipoPropiedad(TipoPropiedad tipoPropiedad) {
 		this.tipoPropiedad = tipoPropiedad;
 	}
-	
-	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 	public TipoOperacion getTipoOperacion() {
 		return tipoOperacion;
 	}
-
 	public void setTipoOperacion(TipoOperacion tipoOperacion) {
 		this.tipoOperacion = tipoOperacion;
 	}
-
-	public String getNombreConstructora(){
-		return constructora.getNombreConstructora();
+	public String getDisponible() {
+		return disponible;
 	}
-
+	public void setDisponible(String disponible) {
+		this.disponible = disponible;
+	}
+	public String getProyecto() {
+		return proyecto;
+	}
+	public void setProyecto(String proyecto) {
+		this.proyecto = proyecto;
+	}
+	public int getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(int metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public int getPrecioUF() {
+		return precioUF;
+	}
+	public void setPrecioUF(int precioUF) {
+		this.precioUF = precioUF;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public PropietarioVO getPropietario() {
+		return propietario;
+	}
+	public void setPropietario(PropietarioVO propietario) {
+		this.propietario = propietario;
+	}
+	public Map<String, String> getUrlImagenes() {
+		return urlImagenes;
+	}
+	public void setUrlImagenes(Map<String, String> urlImagenes) {
+		this.urlImagenes = urlImagenes;
+	}
 	public enum TipoOperacion{
 		ARRIENDO,VENTA;
 	}
